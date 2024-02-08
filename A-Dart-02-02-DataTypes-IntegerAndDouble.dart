09ca66e16fc6afab4892a7a701dc1f3ff8479115
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-08 19:45:17 +0300

    Create A-Dart-02-02-DataTypes-IntegerAndDouble.dart

---
+++ A-Dart-02-02-DataTypes-IntegerAndDouble.dart
@@ -0,0 +1,12 @@
+//integer and double data types
+
+//main.dart file
+//open terminal and run "dart main.dart"
+
+void main() {
+  int thisYear = 2024;
+
+  double pi = 3.14;
+
+  int herAge = 34;
+}
