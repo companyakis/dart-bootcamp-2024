a2b689c62c96487556e16d0528dc4715450c3027
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-08 20:00:28 +0300

    Create A-Dart-02-04-VarKeyword.dart

---
+++ A-Dart-02-04-VarKeyword.dart
@@ -0,0 +1,12 @@
+void main() {
+  
+  var myName = "Mustafa";
+
+  var myCountry = "Turkiye";
+
+  var myAge = 126;
+
+  //myName = 45; //A value of type 'int' can't be assigned to a variable of type 'String'
+
+  //myAge = "17"; //A value of type 'String' can't be assigned to a variable of type 'int'
+}
