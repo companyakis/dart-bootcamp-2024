8c5f6c0b52ad9f444cb02e8fc15d9eab639cf21a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-08 19:41:13 +0300

    Create A-Dart-02-01-DataTypes-String.dart

---
+++ A-Dart-02-01-DataTypes-String.dart
@@ -0,0 +1,14 @@
+//String data type
+
+//main.dart file
+//open terminal and run "dart main.dart"
+
+void main() {
+  String firstMessage = "A rolling stone gathers no moss!";
+
+  print(firstMessage); //A rolling stone gathers no moss!
+
+  String myName = "mustafa";
+
+  print("Welcome, ${myName.toUpperCase()}!"); //Welcome, MUSTAFA!
+}
