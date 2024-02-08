8b94287dcfa204b2c4085e2a4637278fc907be08
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-08 20:11:16 +0300

    Create A-Dart-02-06-Dynamic.dart

---
+++ A-Dart-02-06-Dynamic.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  //dynamic
+  
+  dynamic thisVariableCanHaveDifferentDataTypes = "Mustafa";
+
+  thisVariableCanHaveDifferentDataTypes = true;
+
+  thisVariableCanHaveDifferentDataTypes = 15.254;
+
+  thisVariableCanHaveDifferentDataTypes = 2024;
+
+  print(thisVariableCanHaveDifferentDataTypes); //2024
+}
