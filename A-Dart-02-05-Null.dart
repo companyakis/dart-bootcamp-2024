8d120766686a5b3567ceaa3c38e3bd2f2a0fa415
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-08 20:04:42 +0300

    Create A-Dart-02-05-Null.dart

---
+++ A-Dart-02-05-Null.dart
@@ -0,0 +1,7 @@
+void main() {
+  
+  //null
+  String iWillTellYouASecret;
+
+  print(iWillTellYouASecret); //Error: Non-nullable variable 'iWillTellYouASecret' must be assigned before it can be used.
+}
