da9766741ea583b0f6038cb548097d3b0bd4c9ce
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-08 19:49:57 +0300

    Create A-Dart-02-03-DataTypes-Boolean.dart

---
+++ A-Dart-02-03-DataTypes-Boolean.dart
@@ -0,0 +1,15 @@
+//boolean data type
+
+void main() {
+  bool amIHappy = true;
+
+  bool isCompleted = false;
+
+  bool comparision = 12 > 3;
+
+  print(comparision); //true
+
+  bool comparision2 = 4 >= 112;
+
+  print(comparision2); //false
+}
