d1efaf76e766b09db602d640902684badc9bb5d1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 10:04:04 +0300

    Create A-Dart-06-Sets.dart

---
+++ A-Dart-06-Sets.dart
@@ -0,0 +1,22 @@
+void main() {
+  
+  List<int> numbers = [101, 212, -5, 17, 17, 17, 101];
+  
+  print(numbers);
+  print("\n");
+  
+  Set<int> numbersSet = {101, 212, -5, 17, 17, 17, 101};
+  
+   print(numbersSet);
+  
+  }
+
+  /*
+
+[101, 212, -5, 17, 17, 17, 101]
+
+
+{101, 212, -5, 17}
+
+  */
+
