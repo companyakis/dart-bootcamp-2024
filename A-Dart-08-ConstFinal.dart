6da3b9caad3188d2b973c719b05a6109d1049e0e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 10:46:39 +0300

    Create A-Dart-08-ConstFinal.dart

---
+++ A-Dart-08-ConstFinal.dart
@@ -0,0 +1,25 @@
+void main() {
+  
+  int radius1 = 4;
+  
+  final double pi1 = 3.14; 
+  
+  final square1 = pi1 * radius1 * radius1;
+  
+  print(square1); //50.24
+  
+
+  const double pi2 = 3.14;
+
+  //int radius2 = 4;
+  
+  //const square2 = pi2 * radius2 * radius2; //Error: Not a constant expression.
+  
+  const int radius2 = 4;
+  
+  const square2 = pi2 * radius2 * radius2;
+
+  print(square2); //50.24
+
+  
+  }
