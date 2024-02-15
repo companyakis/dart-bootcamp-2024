96308a395917b636b78d5a36ba4e3efb129d0306
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-15 10:19:54 +0300

    Create A-Dart-07-Maps.dart

---
+++ A-Dart-07-Maps.dart
@@ -0,0 +1,20 @@
+//key-value
+
+void main() {
+  
+  Map<String, int> ages = {
+    "Bilge" : 21,
+    "Aygün" : 34,
+    "Mustafa" : 59,
+    "Sevda" : 77
+  };
+  
+  print("Sevda is ${ages["Sevda"]} years old.");
+  
+  }
+
+  /*
+Sevda is 77 years old.
+
+  */
+
