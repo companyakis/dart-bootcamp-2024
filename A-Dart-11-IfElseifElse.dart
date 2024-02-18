2a8c89f08ae893bbd68aca2aeb42958075225fe6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-19 00:06:45 +0300

    Create A-Dart-11-IfElseifElse.dart

---
+++ A-Dart-11-IfElseifElse.dart
@@ -0,0 +1,13 @@
+void main() {
+  
+  int a = -654;
+  
+  if ( a > 0) {
+    print("Number a is positive.");
+  } else if (a < 0) {
+    print("Number a is negative.");
+  } else {
+    print("Number a is 0.");
+  }
+
+}
