c23632066d0a7d2b27c8aaf86dcc624dac236503
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-20 23:55:19 +0300

    Create A-Dart-14-ForLoop.dart

---
+++ A-Dart-14-ForLoop.dart
@@ -0,0 +1,22 @@
+void main(){
+    
+    for (int i = 0; i < 10; i++) {
+        print("Sayı_${i + 1}: $i");
+    }
+
+}
+
+/*
+
+Sayı_1: 0
+Sayı_2: 1
+Sayı_3: 2
+Sayı_4: 3
+Sayı_5: 4
+Sayı_6: 5
+Sayı_7: 6
+Sayı_8: 7
+Sayı_9: 8
+Sayı_10: 9
+
+*/
