89ee0ddcfb38cc137b465c274b094497db9e153f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-21 00:10:20 +0300

    Create A-Dart-16-WhileLoop.dart

---
+++ A-Dart-16-WhileLoop.dart
@@ -0,0 +1,26 @@
+void main(){
+    
+    int counter = 9;
+    int i = 0;
+    
+    while (counter >= 0) {
+        
+        print("Number_${i + 1}: $counter");
+        counter--;
+        i++;
+    }
+ 
+}
+
+/*
+Number_1: 9
+Number_2: 8
+Number_3: 7
+Number_4: 6
+Number_5: 5
+Number_6: 4
+Number_7: 3
+Number_8: 2
+Number_9: 1
+Number_10: 0
+*/
