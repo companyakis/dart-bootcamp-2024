b9efb8eb2d393af6f0276f5665783f1fa4dee01d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-21 20:35:01 +0300

    Create A-Dart-19-NullSafety5.dart

---
+++ A-Dart-19-NullSafety5.dart
@@ -0,0 +1,21 @@
+void main(){
+    
+
+    
+}
+
+class Employee {
+    String name;
+    int id;
+}
+
+
+ 
+
+/*
+jdoodle.dart:8:12: Error: Field 'name' should be initialized because its type 'String' doesn't allow null.
+    String name;
+           ^^^^
+jdoodle.dart:9:9: Error: Field 'id' should be initialized because its type 'int' doesn't allow null.
+    int id;
+*/
