565b0c71c605731d1e5eaaed5988e56a455c149e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-27 09:50:41 +0300

    Create C-OOP-01-04-ClassScopeThis.dart

---
+++ C-OOP-01-04-ClassScopeThis.dart
@@ -0,0 +1,13 @@
+//main.dart file
+
+import 'scope.dart';
+
+void main() {
+  Scope birthYearTest = Scope();
+
+  birthYearTest.printBirthYear(); //1990! Why? We can say "This" keyword represents class...
+
+  //run dart main.dart
+
+  
+}
