f66e0bbb1404ac151ea942f2087d6d4580b4b064
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-27 09:25:20 +0300

    Create C-OOP-01-01-CreateClass.dart

---
+++ C-OOP-01-01-CreateClass.dart
@@ -0,0 +1,12 @@
+//employee.dart file
+
+class Employee {
+  ///This class holds employee information
+  ///Name, ID, Email, Phone and Department information
+
+  late String name;
+  late int id;
+  late String email;
+  late String phone;
+  late String department;
+}
