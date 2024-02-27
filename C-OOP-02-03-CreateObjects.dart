094423e68c12cd9e6065f2e3b4e594122d120d72
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-27 10:55:32 +0300

    Create C-OOP-02-03-CreateObjects.dart

---
+++ C-OOP-02-03-CreateObjects.dart
@@ -0,0 +1,10 @@
+//main.dart file
+
+import 'employee.dart';
+
+void main() {
+  Employee mustafa =
+      Employee("Mustafa", 1, "mustafa@mynet.com", "05051112223", "Founder");
+
+  print(mustafa.email);
+}
