e000a7c611244d28a346ecc41c9eb1dee5fc76ec
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-27 09:26:26 +0300

    Create C-OOP-01-02-CreateObject.dart

---
+++ C-OOP-01-02-CreateObject.dart
@@ -0,0 +1,12 @@
+//main.dart file
+import 'employee.dart';
+
+void main() {
+  Employee bilge = Employee();
+
+  bilge.name = "Bilge";
+  bilge.id = 4296;
+  bilge.email = "bilge.ucar@companyakiss.com";
+  bilge.phone = "5462142111";
+  bilge.department = "Budget";
+}
