7c212dfe60aa312283741e919bb8eab023956831
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-27 11:42:14 +0300

    Create C-OOP-02-05-Required.dart

---
+++ C-OOP-02-05-Required.dart
@@ -0,0 +1,12 @@
+//main.dart file
+
+import 'employee.dart';
+
+void main() {
+  Employee mustafa = Employee(
+      name: "Mustafa",
+      id: 1,
+      email: "mustafa@yahoo.com",
+      phone: "505 555 33 22",
+      department: "Founder");
+}
