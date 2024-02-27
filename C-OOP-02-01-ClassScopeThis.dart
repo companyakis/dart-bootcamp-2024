9cecee761cf484bf1954cff8c25f8154f79f0759
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-27 09:49:10 +0300

    Create C-OOP-02-01-ClassScopeThis.dart

---
+++ C-OOP-02-01-ClassScopeThis.dart
@@ -0,0 +1,21 @@
+//scope.dart file
+
+//which birth year will be printed?
+
+class Scope {
+  int birthYear = 1990;
+
+  void printBirthYear() {
+    int birthYear = 1995;
+
+    for (int i = 0; i < 21; i += 3) {
+      int birtthYear = 1975;
+
+      if (i == 0) {
+        int birthYear = 1970;
+
+        print(this.birthYear);
+      }
+    }
+  }
+}
