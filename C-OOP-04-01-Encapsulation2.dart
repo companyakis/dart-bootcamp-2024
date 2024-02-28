703a3c63ebae5036e2b1f47cbac5f8318fa269a7
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-28 19:50:07 +0300

    Create C-OOP-04-01-Encapsulation2.dart

---
+++ C-OOP-04-01-Encapsulation2.dart
@@ -0,0 +1,13 @@
+//main.dart file
+
+import 'employee.dart';
+
+void main() {
+ 
+  //Where is wage increase?
+  
+  //Encapsulation!
+  
+  Employee bilge = Employee("Bilge", 102, "x@yy.com", "555222", "HR", 15000.5);
+  
+}
