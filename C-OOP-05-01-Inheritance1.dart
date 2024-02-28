4f4b1f74d70d5445c7ed2efa9098435e9378b800
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-28 20:36:27 +0300

    Create C-OOP-05-01-Inheritance1.dart

---
+++ C-OOP-05-01-Inheritance1.dart
@@ -0,0 +1,13 @@
+//employee.dart file
+
+class Employee {
+  String name;
+  int id;
+  String email;
+  String phone;
+  String department;
+  double wage;
+
+  Employee(
+      this.name, this.id, this.email, this.phone, this.department, this.wage);
+}
