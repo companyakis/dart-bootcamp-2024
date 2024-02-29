894648aa1710f4a9b0ce4ea3202ca37cbde75f5e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-29 20:20:14 +0300

    Create C-OOP-06-FunctionalClass1.dart

---
+++ C-OOP-06-FunctionalClass1.dart
@@ -0,0 +1,19 @@
+//calculus.dart file
+
+class Calculus {
+  double sum(double a, double b) {
+    return a + b;
+  }
+
+  double diff(double a, double b) {
+    return a - b;
+  }
+
+  double mult(double a, double b) {
+    return a * b;
+  }
+
+  double divide(double a, double b) {
+    return a / b;
+  }
+}
