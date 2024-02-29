432b628b67e1534df2cd94fb1778f4aab8390aa2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-02-29 19:55:31 +0300

    Create C-OOP-05-02-Polymorphism1.dart

---
+++ C-OOP-05-02-Polymorphism1.dart
@@ -0,0 +1,14 @@
+//geometricshapes.dart file
+
+class GeometricShapes {
+  double height;
+  double weight;
+
+  GeometricShapes(this.height, this.weight);
+
+  //define a class method
+
+  double calculateArea() {
+    return height * weight;
+  }
+}
