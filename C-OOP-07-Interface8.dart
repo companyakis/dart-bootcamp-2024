cf6cd2c84965b8973c0bfa8e8fae6596eaf4af62
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-03 22:47:58 +0300

    Create C-OOP-07-Interface8.dart

---
+++ C-OOP-07-Interface8.dart
@@ -0,0 +1,20 @@
+//main.dart file
+
+import 'interface_folders/baby.dart';
+
+void main() {
+  Baby aBaby = Baby();
+
+  aBaby.drink();
+  aBaby.eat();
+  aBaby.sleep();
+  aBaby.speak();
+}
+
+/*
+The baby drinks.
+The baby eats.
+The baby sleeps.
+The baby speaks.
+
+*/
