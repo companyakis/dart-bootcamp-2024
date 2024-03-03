ad608dfdfc24a1611cd438621a55bf81e7d2ae2b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-03 20:25:44 +0300

    Create C-OOP-07-Static.dart

---
+++ C-OOP-07-Static.dart
@@ -0,0 +1,26 @@
+//calculus.dart file
+
+//static usage
+
+class Calculus {
+  
+  static const double pi = 3.14;
+
+  static const int startYear = 1923;
+
+  static double sum(double a, double b) {
+    return a + b;
+  }
+
+  static diff(double a, double b) {
+    return a - b;
+  }
+
+  static mult(double a, double b) {
+    return a * b;
+  }
+
+  static divide(double a, double b) {
+    return a / b;
+  }
+}
