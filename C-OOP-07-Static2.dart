37a72193e319b2f4b2b5cc086f3cb12d551b9613
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-03 20:28:51 +0300

    Create C-OOP-07-Static2.dart

---
+++ C-OOP-07-Static2.dart
@@ -0,0 +1,14 @@
+//main.dart file
+
+import 'calculus.dart';
+
+void main() {
+  
+  //We can use class directly now
+
+  print(Calculus.startYear); //1923
+
+  print(Calculus.pi); //3.14
+
+  print(Calculus.mult(25, 42)); //1050.0
+}
