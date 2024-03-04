b758aeff7ccda156157bcb3a4bfd8099200c0182
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-05 00:21:31 +0300

    Create C-OOP-09-Enum2.dart

---
+++ C-OOP-09-Enum2.dart
@@ -0,0 +1,9 @@
+//main.dart file
+import 'employee.dart';
+
+void main() {
+  
+  Employee bilge = Employee(1, "Bilge", Departments.finance);
+
+  Employee aygun = Employee(45, "Aygün", Departments.hr);
+}
